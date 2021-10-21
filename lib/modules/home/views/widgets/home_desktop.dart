936f9e4c39675bf92f71f81bff8bc8461a9d89ca
Roshan Sinha <roshan.sinha@customtravelsolutions.com>
2021-10-21 05:23:16 -0700

    Web Part working

--- lib/modules/home/views/widgets/home_desktop.dart
+++ lib/modules/home/views/widgets/home_desktop.dart
@@ -7,7 +7,14 @@ class HomeDesktop extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Desktop'),
+        title: const Text('Desktop (Web)'),
+      ),
+      body: const Image(
+        image: AssetImage('assets/img/backgroundimage.png'),
+        alignment: Alignment.center,
+        height: double.infinity,
+        width: double.infinity,
+        fit: BoxFit.fill,
       ),
     );
   }
