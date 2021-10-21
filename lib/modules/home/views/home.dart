16dc9eec5c3b220772bc7c0fd3eb0b8045b30adb
Roshan Sinha <roshan.sinha@customtravelsolutions.com>
2021-10-21 04:10:53 -0700

    First init

--- lib/modules/home/views/home.dart
+++ lib/modules/home/views/home.dart
@@ -1,29 +1,15 @@
+import 'package:cts_poc/widgets/responsive.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../auth/controller/auth_notifier.dart';
-import '../../../widgets/loading_indicator.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-        actions: [
-          Consumer(builder: (context, ref, child) {
-            return IconButton(
-              tooltip: 'Sign Out',
-              onPressed: () {
-                LoadingIndicator.of(context)
-                    .during(ref.read(authNotifier.notifier).signout());
-              },
-              icon: const Icon(Icons.exit_to_app_outlined),
-            );
-          })
-        ],
-      ),
+    return Responsive(
+      mobile: Scaffold(),
+      tablet: Scaffold(),
+      desktop: Scaffold(),
     );
   }
 }
