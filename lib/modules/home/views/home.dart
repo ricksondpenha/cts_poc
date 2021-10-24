4cc5cf2829789c01a85ab8bf3966e8bd7baa027b
Rickson Dpenha <ricksondpenha@gmail.com>
2021-10-25 01:27:54 +0530

    added text

--- lib/modules/home/views/home.dart
+++ lib/modules/home/views/home.dart
@@ -20,6 +20,7 @@ class Home extends StatelessWidget {
             child: Column(
               children: [
                 Container(
+                  alignment: Alignment.center,
                   height: MediaQuery.of(context).size.height,
                   width: double.infinity,
                   decoration: const BoxDecoration(
@@ -27,6 +28,34 @@ class Home extends StatelessWidget {
                           fit: BoxFit.cover,
                           image: NetworkImage(
                               'https://picsum.photos/id/1011/1080/763'))),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        'LIFELONG',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22,
+                        ),
+                      ),
+                      Text(
+                        'MEMORIES',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 48,
+                        ),
+                      ),
+                      Text(
+                        'Just a few seconds away',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
